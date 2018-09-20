e5ebdd3f870e0db5d0d4792a0c37f35a02cfa32a
Cedric David <cedric.david@btopenworld.com>
2018-09-20 12:55:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ class App < Sinatra::Base
       # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
-      "Session ID has not been set!"
+      session[:id] = 1
     end
   end
 
